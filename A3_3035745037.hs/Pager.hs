5748e95a843287a51576f7c84460a3f51c96edd3
Ch1ckenMax <li-hk@hotmail.com>
2023-11-16 20:26:53 +0800

    finished A3

--- A3_3035745037.hs/Pager.hs
+++ A3_3035745037.hs/Pager.hs
@@ -8,12 +8,12 @@ import Data.List
 
 runPager :: IO ()
 runPager = do
-           filename <- handleArgs
-           contents <- readFile filename
+           filenames <- handleArgs
+           contents <- readFiles filenames
            termSize <- getTerminalSize
-           let pages                      = paginate termSize contents
+           let fileLines                  = paginateFiles termSize contents
                ScreenDimensions rows cols = termSize
-           showPages filename (rows - 1) pages
+           showPages filenames (rows - 1) fileLines
 
 main = runPager 
 
@@ -39,9 +39,15 @@ getTerminalSize = case SystemInfo.os of
 clearScreen :: IO ()
 clearScreen = putStr "\^[[1J\^[[1;1H"
 
-handleArgs :: IO FilePath
-handleArgs = do args <- getArgs 
-                return $ head args
+handleArgs :: IO [FilePath]
+handleArgs = getArgs 
+
+-- Given a list of file paths, read the files and return a list of strings
+readFiles :: [FilePath] -> IO [String]
+readFiles []     = return []
+readFiles (f:fs) = do s  <- readFile f
+                      ss <- readFiles fs
+                      return (s:ss) 
 
 wordWrap :: Int -> String -> [String]
 wordWrap 0 lineText          = []
@@ -49,6 +55,11 @@ wordWrap lineLength lineText = if length lineText <= lineLength then
                                  [lineText] 
                                else 
                                  take lineLength lineText : wordWrap lineLength (drop lineLength lineText)
+
+-- Given a list of strings, paginate each files and return a list of lists of strings (where each element of the list corresponds to one file)
+paginateFiles :: ScreenDimensions -> [String] -> [[String]]
+paginateFiles scrdim []     = []
+paginateFiles scrdim (f:fs) = paginate scrdim f:paginateFiles scrdim fs
 
 paginate :: ScreenDimensions -> String -> [String]
 paginate (ScreenDimensions rows cols) text = foldr aux [] lines
@@ -58,32 +69,53 @@ paginate (ScreenDimensions rows cols) text = foldr aux [] lines
                                              aux :: String -> [String] -> [String]
                                              aux line wrappedLines = wordWrap lineSize line ++ wrappedLines
                                                
-showPages :: String -> Int -> [String] -> IO ()
-showPages fileName rowsOnScreen lines = do hSetBuffering stdin NoBuffering
-                                           hSetEcho stdin False
-                                           showPage fileName rowsOnScreen lines 0
-               
-
--- Prints one page
-showPage :: String -> Int -> [String] -> Int -> IO ()
-showPage fileName rowsOnScreen lines currentPage = let totalLines = length lines
-                                                       totalPages = totalLines `div` rowsOnScreen + if totalLines `mod` rowsOnScreen == 0 then 0 else 1
-                                                       startLine  = if totalPages == 2 && currentPage == 1 then totalLines - rowsOnScreen else currentPage * rowsOnScreen
-                                                       endLine    = min (startLine + rowsOnScreen) totalLines
-                                                       pageStats  = (" Page:" ++ show (currentPage + 1) ++ "/" ++ show totalPages)
-                                                       lineStats  = (" Lines:" ++ show (startLine + 1) ++ "~" ++ show endLine ++ "/" ++ show totalLines) 
-                                                       showPage'  = showPage fileName rowsOnScreen lines in
-                                                    do clearScreen
-                                                       printLines lines startLine endLine
-                                                       putStr (fileName ++ pageStats ++ lineStats)
-                                                       input <- getChar
-                                                       case input of 
-                                                         ' ' -> showPage' $ min (currentPage + 1) (totalPages - 1) 
-                                                         'b' -> showPage' $ max (currentPage - 1) 0
-                                                         'q' -> do putStrLn ""
-                                                                   return ()
-                                                         _   -> showPage' currentPage
-
+showPages :: [String] -> Int -> [[String]] -> IO ()
+showPages fileNames rowsOnScreen fileLines = do hSetBuffering stdin NoBuffering
+                                                hSetEcho stdin False
+                                                showPage 0 0
+  where 
+    totalFiles = length fileNames
+    showPage :: Int -> Int -> IO ()
+    showPage currentFile currentPage = let -- Get the data of the current file
+                                           lines      = fileLines !! currentFile
+                                           fileName   = fileNames !! currentFile
+                                           totalLines = length lines
+                                           totalPages = totalLines `div` rowsOnScreen + if totalLines `mod` rowsOnScreen == 0 then 0 else 1
+                                           startLine  = if totalPages > 1 && totalPages == currentPage + 1 then totalLines - rowsOnScreen else currentPage * rowsOnScreen
+                                           endLine    = min (startLine + rowsOnScreen) totalLines
+                                           -- Status bar strings
+                                           pageStats  = (" Page:" ++ show (currentPage + 1) ++ "/" ++ show totalPages)
+                                           lineStats  = (" Lines:" ++ show (startLine + 1) ++ "~" ++ show endLine ++ "/" ++ show totalLines) 
+                                           fileStats  = (" Files:" ++ show (currentFile + 1) ++ "/" ++ show totalFiles) in 
+                                       do clearScreen
+                                          printLines lines startLine endLine  
+                                          putStr (fileName ++ pageStats ++ lineStats ++ fileStats)
+                                          input <- getChar
+                                          case input of 
+                                            ' ' -> if currentPage + 1 >= totalPages then -- Next file
+                                                     if currentFile + 1 < totalFiles then
+                                                       showPage (currentFile + 1) 0
+                                                     else -- Current file is already the last file
+                                                       showPage currentFile currentPage
+                                                   else -- Current file
+                                                     showPage currentFile (currentPage + 1)  
+                                            'b' -> if currentPage - 1 < 0 then -- Previous file
+                                                     if currentFile - 1 >= 0 then 
+                                                       showPage (currentFile - 1) $ getLastPageOfFile (currentFile - 1)
+                                                     else -- Current file is already the first file
+                                                       showPage currentFile currentPage
+                                                   else
+                                                     showPage currentFile (currentPage - 1)
+                                            'q' -> do clearScreen
+                                                      return ()
+                                            _   -> showPage currentFile currentPage  
+    -- Given an index of file, find the index of its last page
+    getLastPageOfFile :: Int -> Int
+    getLastPageOfFile file = totalPages - 1
+      where 
+        totalLines = length $ fileLines !! file
+        totalPages = totalLines `div` rowsOnScreen + if totalLines `mod` rowsOnScreen == 0 then 0 else 1
+                       
 -- Prints the [start, end) lines (starting index at 0)
 printLines :: [String] -> Int -> Int -> IO ()
 printLines lines start end | start == end          = return () -- Nothing needs to be printed
